e509c56c8607e389455c1bc95c7d15da3e7bcf66
mdalvi <milind.dalvi14@gmail.com>
2021-01-05 22:55:10 +0530

    basic skeleton up

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='junkyard',
+    version='1.0',
+    packages=['maintained', 'unmaintained'],
+    install_requires=['numpy'],
+    url='https://turingequations.com',
+    license='GNU GENERAL PUBLIC LICENSE 3',
+    author='mdalvi',
+    author_email='milind.dalvi@turingequations.com',
+    description=''
+)
